3985d6102127ac4506c05773f6e4c1d15fa49f4d
kish1n <yehorushakov@gmail.com>
2024-12-26 11:15:44 +0200

    fix version

--- mailbox/check_and_delete_in_box.go
+++ mailbox/check_and_delete_in_box.go
@@ -6,18 +6,13 @@ func (m *Service) CheckAndDeleteInBox(email string, ConfidenceCode string, opera
 
 	if operations, exists := m.listCode[email]; exists {
 		if data, opExists := operations[operationType]; opExists {
-
 			if data.ConfidenceCode == ConfidenceCode && data.Meta.UserAgent == UserAgent && data.Meta.IP == IP {
-
 				delete(operations, operationType)
-
 				if len(operations) == 0 {
 					delete(m.listCode, email)
 				}
-
 				return true
 			}
-
 		}
 	}
 
